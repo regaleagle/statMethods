63714be0c52e4b187fc768b83b6236f2e48d6de5
regaleagle <guseagja@student.gu.se>
2017-11-24 11:03:08 +0800

    load data as per instructions

---
+++ statMethods3/statMethods3.py
@@ -0,0 +1,21 @@
+from __future__ import division
+from codecs import open
+
+def read_documents(doc_file):
+    docs = []
+    labels = []
+    with open(doc_file, encoding='utf-8') as f:
+        for line in f:
+            words = line.strip().split()
+            docs.append(words[3:])
+            labels.append(words[1])
+    return docs, labels
+
+all_docs, all_labels = read_documents('all_sentiment_shuffled.txt')
+
+split_point = int(0.80*len(all_docs))
+train_docs = all_docs[:split_point]
+train_labels = all_labels[:split_point]
+eval_docs = all_docs[split_point:]
+eval_labels = all_labels[split_point:]
+
