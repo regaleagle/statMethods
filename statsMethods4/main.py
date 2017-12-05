b61f91857cc3fa40989341c5307d506aa4489b30
regaleagle <guseagja@student.gu.se>
2017-12-06 01:15:46 +1100

    changes to task 1 and 3

--- statsMethods4/main.py
+++ statsMethods4/main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas
+import numpy as np
 from math import *
 from pomegranate import *
 
@@ -13,11 +14,28 @@ hyb_data = pandas.read_csv('hyb.txt', sep='\t', header=None)
 
 ## Task 1
 # RATIOS = CHROMOSOMAL_POS_COL. There is absolutely no way to know this other than to ask someone who already knows.
+# Are we sure about this? to me it looks like the ratios are FIRST_PATIENT_COL at the chromosomal position - James
+#
 plt.plot(hyb_data[CHROMOSOMAL_POS_COL], hyb_data[FIRST_PATIENT_COL])
+plt.xlabel('position')
+plt.ylabel('ratio')
+plt.ylim(-0.5,1) #Looks better but maybe bad? - James
+
+#Highlighting possible segments
+plt.axvspan(0, 7200000, color='red', alpha=0.5)
+plt.axvspan(7200000, 32000000, color='blue', alpha=0.5)
+plt.axvspan(78000000, 120000000, color='red', alpha=0.5)
+plt.axvspan(167000000, 182000000, color='blue', alpha=0.5)
+plt.axvspan(182000000, 213000000, color='red', alpha=0.5)
+plt.axvspan(248000000, 270000000, color='red', alpha=0.5)
+plt.axvspan(276000000, 307000000, color='blue', alpha=0.5)
 plt.show()
 
-plt.hist(hyb_data[CHROMOSOMAL_POS_COL])
+plt.hist(hyb_data[FIRST_PATIENT_COL], range=(-1,1), bins=30)
+#Looks a little nicer with the range, no idea if it is a good idea -James
 plt.show()
+
+#Does anyone know what he means by thresholds? -James
 
 ## Task 2
 # INPUT:
@@ -38,10 +56,11 @@ plt.show()
 
 def create_hmm(means_and_variances, transition_matrix):
     states = []
-    
+    count = 0;
     for mean, variance in means_and_variances:
-        state = State(NormalDistribution(mean, sqrt(variance)))
+        state = State(NormalDistribution(mean, sqrt(variance)), name="S"+str(count))
         states.append(state)
+        count += 1
     
     model = HiddenMarkovModel()
     model.add_states(states)
@@ -57,19 +76,41 @@ def create_hmm(means_and_variances, transition_matrix):
     return model
 
 ## Task 3
-def seg_print(sequences, states, sequence_length):
+def seg_print(sequences, states):
     for i,s in enumerate(sequences):
         print("Data {}: {}".format(i, "".join(str(o) for o in s)))
         print("State: {}".format(states[i][0])) # As our states dont haeve names, use the index
 
+def create_seg_plot(sequence, t, path):
+    plt.plot(t, sequence)
+    plt.xlabel('position')
+    plt.ylabel('ratio')
+    plt.ylim(-0.5, 1)
+    start = 0
+    if path[0] == "None - start":
+        path.pop(0)
+    current_state = path[0]
+    for i in range(len(t)):
+        if path[i][1].name != current_state:
+
+            if path[i][1].name == 'S0':
+                plt.axvspan(start, t[i], color='red', alpha=0.5)
+            elif path[i][1].name == 'S2':
+                plt.axvspan(start, t[i], color='blue', alpha=0.5)
+
+            current_state = path[i][1].name
+            start = t[i]
+    plt.show()
+
 def train_model(model, sequences):
-    model.fit(sequences) # Baum-welch training
+    model.fit([sequences]) # Baum-welch training
+    logp, path = model.viterbi(sequences) # Viterby segment
+    return path
 
-    segment = model.viterbi(sequences) # Viterby segment
-    seg_print(sequences, segment[1], len(sequences[0]))
+hmm_model = create_hmm([(-0.1, .05), (0.0, 0.05), (0.1, 0.05)],
+                                                    [[0.3,0.3,0.3],
+                                                    [0.3,0.3,0.3],
+                                                    [0.3,0.3,0.3]])
 
-hmm_model = create_hmm([(0, 1), (1, 1), (2, 1)],   [[0.2,0.3,0.6],
-                                                    [0.1,0.8,0.2],
-                                                    [0.9,0.6,0.8]])
-
-train_model(hmm_model, [[0, 1, 1, 0, 2, 1], [1, 2, 1, 1, 1, 1], [2, 2, 2, 1, 1, 1]])
+path = train_model(hmm_model, hyb_data[FIRST_PATIENT_COL])
+create_seg_plot(hyb_data[FIRST_PATIENT_COL], hyb_data[CHROMOSOMAL_POS_COL], path)
