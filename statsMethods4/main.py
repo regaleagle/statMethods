53f50fb56a28d92632e2680fba34b01c24f454ac
regaleagle <guseagja@student.gu.se>
2017-12-07 14:47:02 +1100

    fix some rounding errors that were breaking behaviour

--- statsMethods4/main.py
+++ statsMethods4/main.py
@@ -21,7 +21,8 @@ first_patient_ratio = np.array(hyb_data[FIRST_PATIENT_COL])
 # RATIOS = CHROMOSOMAL_POS_COL. There is absolutely no way to know this other than to ask someone who already knows.
 # Are we sure about this? to me it looks like the ratios are FIRST_PATIENT_COL at the chromosomal position - James
 #
-plt.scatter(chromo_pos, first_patient_ratio)
+plt.scatter(chromo_pos, first_patient_ratio, s=2)
+plt.title('Best Guess Segmentation')
 plt.xlabel('position')
 plt.ylabel('ratio')
 plt.ylim(-0.5,1) #Looks better but maybe bad? - James
@@ -45,46 +46,52 @@ plt.show()
 
 ## Task 2
 # INPUT:
-#   means_and_variances: 
+#   means_and_variances:
 #       List of states with their Guassian PDF means and variances: [(mean, variance), (mean, variance) ....]
 #
-#   transitions: 
+#   transitions:
 #       Transition matrix such that, for the transition between the state 0 and state 1 will be located at row 0 col 1
 #           [0.1    _0.5_      0.6]
 #           [0.3     0.1       0.3]
 #           [0.9     0.4       0.2]
-#   
+#
 #       NOTE: If col value is -1, we assume it means model.end (i.e. the transition between the final state and the end of the model)
 #
 #
 # RETURNS:
 #   Trained model and the generated states (in order to enable path calculations etc)
 
-def create_hmm(means_and_variances, transition_matrix):
+def create_hmm(means_and_stds, transition_matrix):
     states = []
     count = 0;
-    for mean, variance in means_and_variances:
-        state = State(NormalDistribution(mean, sqrt(variance)), name="S"+str(count))
+    for mean, variance in means_and_stds:
+        state = State(NormalDistribution(mean, variance), name="S"+str(count))
         states.append(state)
         count += 1
     model = HiddenMarkovModel()
     model.add_states(states)
 
-    # TODO: Transitions, HOW CAN WE KNOW!??!
-    model.add_transition(model.start, states[0], 0.33) # GUESS: I suppose we know for sure that the first state is reached for certain.
-    model.add_transition(model.start, states[1], 0.33)
-    model.add_transition(model.start, states[2], 0.33)
+    for state in states:
+        model.add_transition(model.start, state, 0.33)
+
+        # Not sure whether we should define state end and
+        # whether or not we should randomize entry or just define 1 start state?
 
     for row in range(len(transition_matrix)):
         for col in range(len(transition_matrix[row])):
             model.add_transition(states[row], states[col], transition_matrix[row][col])
     model.bake()
-    #np.random.seed(int(time.time() // 1000))
+    np.random.seed(int(time.time() // 1000))
     return model
 
 def create_seg_plot(sequence, t, path, initial_params):
-    plt.plot(t, sequence)
-    plt.title("Init_Params" + "\n".join(wrap(str(initial_params), 50)))
+    plt.scatter(t, sequence, s=2)
+    title = ""
+    count = 0
+    for param in initial_params:
+        title += "\n" + f'S{count}: M {param[0]}, Std {param[1]}'
+        count += 1
+    plt.title(title)
     plt.xlabel('position')
     plt.ylabel('ratio')
     plt.ylim(-0.5, 1)
@@ -103,21 +110,23 @@ def create_seg_plot(sequence, t, path, initial_params):
     plt.show()
 
 def train_model(model, sequences):
-    model.fit([sequences], max_iterations=10000) # Baum-welch training by default
+    model.fit([sequences]) # Baum-welch training by default
     logp, path = model.viterbi(sequences) # Viterby segment
     return path
 
 #Task 4
 
 # a) Looking at the histogram, one way we could randomize initial
-# parameters is to take a small sample from different quantile ranges of the data set
+# parameters (instead of taking the uniformly segmented mean and std)
+# is to take a small sample from different quantile ranges of the data set
 # and use the the mean and variance of those samples as our initial Gaussian PDFs.
-# For the transitions we could will simply choose a random distribution from 0-1 for
-# each state (although we will enforce a min value of 0.2 based on trial and error)
+# For the transitions we will simply choose a random distribution from 0-1 for
+# each state's transition row.
 
-# b)After some trial an error we found that sometimes the algorithm can get close to
-# it's max iterations so we limit it at 10000 which seems to give us good results
-#  --Not really sure what he wants here - James
+# b)Using the default Baum-Welch parameters 1e8 max iterations, and 1e-9
+# stop threshold, we usually get results with less than 100 iterations,
+# from this we determined that the stop threshold on improvement was more
+# relevant and the default limit set was fine
 
 # c)While the problem specifically is looking for 3 states, duplication, deletion or norm
 # the data distribution in the initial histogram suggest shows some limited
@@ -126,7 +135,7 @@ def train_model(model, sequences):
 # highlights the extreme data. We've chosen to go with the former to keep our methods generic
 
 def get_initial_state_params(sequence, num_states):
-    means_variances = []
+    means_stds = []
     lower = sequence.min()
     for i in range(1,num_states+1):
         perc = (100/num_states) * i
@@ -134,20 +143,31 @@ def get_initial_state_params(sequence, num_states):
         sample_set = sequence[(sequence > lower) & (sequence < upper)]
         sample = np.random.choice(sample_set, len(sample_set)//4, replace=False)
         lower = upper
-        means_variances.append((sample.mean(), sample.std()))
-    return means_variances
+        means_stds.append((np.around(sample.mean(), 3), np.around(sample.std(), 3)))
+    return means_stds
 
 def get_random_transitions(num_states):
     mat = np.empty(shape=(num_states,num_states))
     for i in range(0,num_states):
-        not_valid = True
-        while not_valid:
-            not_valid = False
-            mat[i] = (np.random.dirichlet(np.ones(num_states),size=1))
-            for elem in mat[i]:
-                if elem <= 0.2:
-                    not_valid = True
+        mat[i] = (np.random.dirichlet(np.ones(num_states),size=1))
+        mat[i] = np.around(mat[i], 2)
+        offset = 1 - np.sum(mat[i])
+        if offset != 0:
+            mat[i][np.random.randint(num_states)] += offset
     return mat
+
+# def get_random_transitions(num_states):
+#     mat = np.empty(shape=(num_states,num_states))
+#     for i in range(0,num_states):
+#         rowtotal = 1
+#         for j in range(0,num_states):
+#             if j == num_states - 1:
+#                 mat[i][j] = rowtotal
+#             else:
+#                 mat[i][j] = numpy.random.randint(rowtotal * 100)/100
+#                 rowtotal -= mat[i][j]
+#     print(mat)
+#     return mat
 
 #Clean
 threshold = np.percentile(first_patient_ratio, 99)
@@ -156,30 +176,25 @@ chromo_pos_clean = chromo_pos[(first_patient_ratio < threshold)]
 
 #Random Parameters
 number_of_states = 3
-means_and_variances = get_initial_state_params(first_patient_ratio_clean, number_of_states)
+means_and_stds = get_initial_state_params(first_patient_ratio_clean, number_of_states)
 trans_mat = get_random_transitions(number_of_states)
 
 #Build Model
-hmm_model = create_hmm(means_and_variances,trans_mat)
+hmm_model = create_hmm(means_and_stds, trans_mat)
 
 # Train and Display
 path = train_model(hmm_model, first_patient_ratio_clean)
-create_seg_plot(first_patient_ratio_clean, chromo_pos_clean, path, means_and_variances)
+create_seg_plot(first_patient_ratio_clean, chromo_pos_clean, path, means_and_stds)
 
 ##Task 5
 
 
-def test_model_prediction(model):
-    # number_of_states_test = 3
-    sample_test_1 = model.sample(length=700, path=True)
-
+def test_model_prediction(m_and_stds, trans_m):
     #Random Parameters Test
-    number_of_states = 3
-    means_and_variances = get_initial_state_params(np.array(sample_test_1[0]), number_of_states)
-    trans_mat = get_random_transitions(number_of_states)
 
     #Build Model Test
-    new_model = create_hmm(means_and_variances,trans_mat)
+    new_model = create_hmm(m_and_stds, trans_m)
+    sample_test_1 = new_model.sample(length=1000, path=True)
 
     #Train and Display Test
     path = train_model(new_model, np.array(sample_test_1[0]))
@@ -195,11 +210,11 @@ def test_model_prediction(model):
     print(100*((total-mismatches)/total))
     return (100*((total-mismatches)/total))
 
-test_model_prediction(hmm_model)
+test_model_prediction(means_and_stds, trans_mat)
 
 # Task 6
 # The posterior plot generated is overall flat with several major dips. This is likely due to some elements of the sequence being very random/unlikely to be next -
-# regardless of which state emits them. If a state has very low in-bound transition probabilities from other states, or if a state has very low out-bound probabilities towards other 
+# regardless of which state emits them. If a state has very low in-bound transition probabilities from other states, or if a state has very low out-bound probabilities towards other
 # states, the 'dip' will be observed. Overall this method does relatively well, however it fails to consider outlying/extreme possibilities.
 
 def compute_posteriors(sequence, model):
@@ -215,9 +230,10 @@ def compute_posteriors(sequence, model):
             posteriors[j] = forward[i][j] + backward[i][j] # As these are log probabilities we will add them as opposed to multiplying them as if they were normal probabilities.
 
         all_posteriors.append(posteriors)
-    
+
     return all_posteriors
 
 posteriors = compute_posteriors(first_patient_ratio_clean, hmm_model)
 plt.plot([max(x) for x in posteriors])
 plt.show()
+
